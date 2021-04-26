bf61a88b2065eb807af210c116364201a909e183
akhanal77 <63509793+akhanal77@users.noreply.github.com>
2021-04-25 22:18:23 -0400

    Update app.sh

--- app.sh
+++ app.sh
@@ -3,8 +3,8 @@ sleep 10
 echo 'Hello World'
 git --version
 sleep 10
-#echo 'Changes made'
-#sleep 1
+echo 'Changes made'
+sleep 10
 #echo 'Second change'
 #echo 'Third change'
 #echo 'Fourth change'
